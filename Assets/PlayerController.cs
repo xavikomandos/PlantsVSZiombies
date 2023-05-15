2e21c5a8f69cc4825f6525da5939bce1267bed1e
Your Name <you@example.com>
2023-05-15 14:44:14 +0200

    hgdhtt

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,34 +1,39 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    float hp = 10;
     public GameObject bulletPrefab;
     public float bulletSpeed = 20;
     public float playerSpeed = 2;
     Vector2 movementVector;
     Transform bulletSpawn;
+    public GameObject hpBar;
+    Scrollbar hpScrollBar;
 
     // Start is called before the first frame update
     void Start()
     {
         movementVector = Vector2.zero;
         bulletSpawn = transform.Find("BulletSpawn");
+        hpScrollBar = hpBar.GetComponent<Scrollbar>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //obrót wokó³ osi Y o iloœæ stopni równ¹ wartosci osi X kontrolera
+        //obrÃ³t wokÃ³Â³ osi Y o iloÅ“Ã¦ stopni rÃ³wnÂ¹ wartosci osi X kontrolera
         transform.Rotate(Vector3.up * movementVector.x);
-        //przesuniêcie do przodu (transform.forward) o wychylenie osi Y kontrolera w czasie jednej klatki
+        //przesuniÃªcie do przodu (transform.forward) o wychylenie osi Y kontrolera w czasie jednej klatki
         transform.Translate(Vector3.forward * movementVector.y * Time.deltaTime * playerSpeed);
     }
-    
-    void OnMove(InputValue inputValue) 
+
+    void OnMove(InputValue inputValue)
     {
         movementVector = inputValue.Get<Vector2>();
 
@@ -39,7 +44,34 @@ public class PlayerController : MonoBehaviour
     {
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawn);
         bullet.transform.parent = null;
-        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*bulletSpeed, ForceMode.VelocityChange);
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * bulletSpeed, ForceMode.VelocityChange);
         Destroy(bullet, 5);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+
+            hp--;
+            if (hp <= 0) Die();
+            hpScrollBar.size = hp / 10;
+            Vector3 pushVector = collision.gameObject.transform.position - transform.position;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(pushVector.normalized * 5, ForceMode.Impulse);
+        }
+        if (collision.gameObject.CompareTag("Heal"))
+        {
+            hp = 10;
+            hpScrollBar.size = hp / 10;
+            Destroy(collision.gameObject);
+        }
+    }
+    void Die()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        transform.Translate(Vector3.up);
+        transform.Rotate(Vector3.right * -90);
+
+        //Time.timeScale = 0;
     }
 }
